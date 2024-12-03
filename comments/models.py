8797eeac2110b7dd478d7f18940cce58088595a0
siggu <m0_w_0m@naver.com>
2024-12-03 21:27:01 +0900

    fix: admin 표기 오류 해결
    commentss -> comments

--- comments/models.py
+++ comments/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Comments(models.Model):
     detail = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "comments"
