f880bb74deaab6bacf9ff39ec74a5ff6ca93f424
siggu <m0_w_0m@naver.com>
2024-12-03 19:19:59 +0900

    feat: bands 필드 추가

--- bands/models.py
+++ bands/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Bands(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.URLField()
+    formation_date = models.CharField(max_length=10)
+    debut_date = models.CharField(max_length=10)
+    genre = models.CharField(max_length=255)
+    members = models.TextField()
+    hit_songs = models.CharField(max_length=255)
+    albums = models.TextField()
+    awards = models.TextField()
+
+    def __str__(self):
+        return self.name
