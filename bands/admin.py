413d39c2e11dfe2d5c22d6fa32a5efdb1df6286a
siggu <m0_w_0m@naver.com>
2024-12-03 19:56:01 +0900

    feat: bands, genre admin 추가

--- bands/admin.py
+++ bands/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Bands
+from .models import Genre
 
 
 @admin.register(Bands)
 class BandAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "hit_songs",
+    )
+    search_fields = (
+        "name",
+        "hit_songs",
+    )
+    filter_horizontal = ("genre",)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
