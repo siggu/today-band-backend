3a344afd7d8797a25b2d1c37b6ffdcdf1d22e62c
siggu <m0_w_0m@naver.com>
2024-12-03 19:54:53 +0900

    feat: Genre 모델 분리
    CharField -> ManyToMany

--- bands/models.py
+++ bands/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Bands(models.Model):
     name = models.CharField(max_length=255)
     photo = models.URLField()
     formation_date = models.CharField(max_length=10)
     debut_date = models.CharField(max_length=10)
-    genre = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre)
     members = models.TextField()
     hit_songs = models.CharField(max_length=255)
     albums = models.TextField()
