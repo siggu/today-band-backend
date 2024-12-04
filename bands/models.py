b7ddfacded691a578cfb6d0b4384aafde4a7ae19
siggu <m0_w_0m@naver.com>
2024-12-04 22:15:13 +0900

    feat: classification 추가
    가나다순 표시 추가

--- bands/models.py
+++ bands/models.py
@@ -8,6 +8,26 @@ class Genre(models.Model):
         return self.name
 
 
+from django.db import models
+
+
+class ClassificationChoices(models.TextChoices):
+    GA = ("ga", "가")
+    NA = ("na", "나")
+    DA = ("da", "다")
+    RA = ("ra", "라")
+    MA = ("ma", "마")
+    BA = ("ba", "바")
+    SA = ("sa", "사")
+    AH = ("ah", "아")
+    JA = ("ja", "자")
+    CHA = ("cha", "차")
+    KA = ("ka", "카")
+    TA = ("ta", "타")
+    PA = ("pa", "파")
+    HA = ("ha", "하")
+
+
 class Band(models.Model):
     name = models.CharField(max_length=255)
     photo = models.URLField()
@@ -18,6 +38,9 @@ class Band(models.Model):
     hit_songs = models.CharField(max_length=255)
     albums = models.TextField()
     awards = models.TextField()
+    classification = models.CharField(
+        max_length=3, choices=ClassificationChoices.choices, default="ga"
+    )
 
     def __str__(self):
         return self.name
